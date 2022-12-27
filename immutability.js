9a598d682647b0c1d28e45bb5b1116d63cdc328d
hans <focus5n@naver.com>
2022-12-27 22:58:25 +0900

    STUDY Freeze
    - Object.freeze(target)

--- immutability.js
+++ immutability.js
@@ -10,4 +10,20 @@ object_02 = immutable_maker(object_01);
 
 console.log("ORIGINAL", object_01);
 console.log("FAKE", object_02);
-console.log(object_01 === object_02);
+console.log("IS EQUAL?", object_01 === object_02);
+console.log("\n==================================\n");
+
+// ========================================= //
+
+let object_03 = {type: 'FREEZE', reference: ['array', 'object']}
+
+Object.freeze(object_03);
+object_03.type = 'CHANGE';
+console.log("FREEZE CAN NOT CHANGE", object_03);
+
+object_03.reference.push('function');
+console.log("FREEZE SAVE ONLY POINTER", object_03);
+
+Object.freeze(object_03.reference);
+// object_03.reference.push('Number');
+console.log("FREEZE PROPERTY OBJECT", object_03);
